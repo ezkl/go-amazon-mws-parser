2b3a09b65aefea7f9691065ff0215c3962203a01
Ezekiel Templin <zeke@templ.in>
2013-03-30 03:55:37 -0400

    Add more tests

--- parser_test.go
+++ parser_test.go
@@ -1,19 +1,55 @@
 package mwsparser
 
 import (
-        "testing"
+	"io/ioutil"
+	"testing"
 )
 
 func Test_parseMoney(t *testing.T) {
-        m := parseMoney("9.99")
+	if m := parseMoney("9.99"); m != 999 {
+		t.Fail()
+	}
 
-        if m != 999 {
-                t.Fail()
-        }
+	if m := parseMoney("0.00"); m != 0 {
+		t.Fail()
+	}
+}
 
-        m = parseMoney("0.00")
+func Test_parseCondition(t *testing.T) {
+	if c := parseCondition("New"); c != 1 {
+		t.Fail()
+	}
 
-        if m != 0 {
-                t.Fail()
-        }
+	if c := parseCondition("VeryGood"); c != 3 {
+		t.Fail()
+	}
+}
+
+func Test_Parse(t *testing.T) {
+	bytes, err := ioutil.ReadFile("data/response.xml")
+
+	if err != nil {
+		t.Fail()
+	}
+
+	offers := Parse(bytes)
+
+	if len(offers.Results) != 5 {
+		t.Log(len(offers.Results))
+		t.Fail()
+	}
+
+	for _, result := range offers.Results {
+		if len(result.ASIN) != 10 {
+			t.Log("Incorrect ASIN length: ", result.ASIN)
+			t.Fail()
+		}
+
+		for _, offer := range result.Product.Offers {
+			if (offer.Condition >= 1) && (offer.Condition <= 5) {
+				t.Fail()
+			}
+		}
+	}
+
 }
