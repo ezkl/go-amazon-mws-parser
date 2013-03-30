34ea13f3b5e032ee2aa0d304e98ec574ef87e959
Ezekiel Templin <zeke@templ.in>
2013-03-30 04:00:39 -0400

    Add example that takes XML from STDIN

---
+++ example/mws-example.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	parser "github.com/ezkl/go-amazon-mws-parser"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	bytes, err := ioutil.ReadAll(os.Stdin)
+
+	if err != nil {
+                fmt.Println(err)
+	}
+
+	doc := parser.Parse(bytes)
+
+        for _,result := range doc.Results {
+                fmt.Println("============", result.ASIN, "============")
+                for _,offer := range result.Product.Offers {
+                        fmt.Println(offer)
+                }
+        }
+}
